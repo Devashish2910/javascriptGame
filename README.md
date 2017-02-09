9fd9d9a70d5df70ec31f26af76f8c5c5029f09a3
Devashish2910 <dgpatel2910@gmail.com>
2017-02-09 02:35:21 -0600

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # javascriptGame
+
+GAME RULES:
+
+- The game has 2 players and there are two dices. 
+- In each turn, a player rolls dices as many times as he whishes. Each result get added to his ROUND score.
+- BUT, if...
+    - a player gets a 1 in any one dice, all his ROUND score gets lost. After that, it's the next player's turn.
+    - a player looses his ENTIRE score when he rolls two 12 (6 + 6 two times) in a row. After that, it's the next player's turn.
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn.
+- The first player to reach Winning Total points on GLOBAL score wins the game. (Players will decide the winning score..!!)
+
--- gameLogic.js
+++ gameLogic.js
@@ -1,34 +1,51 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 //Variables Define
-var playerOneName, playerTwoName, globalScore, localScore, activePlayer, winningTotal, x;
+var playerOneName, playerTwoName, globalScore, localScore, activePlayer, winningTotal, lastLocalScore, dice, dice1, dice2, x;
 
-inquiries();
-init();
+// Check if it's a new game request or reset request
+if (x == "reset") {
+    resetGame();
+}
+else {
+    inquiries();
+    init();
+}
 
-//Roll dice button onclick event
+// 'Roll dice' button onclick event
 document.querySelector('.btn-roll').addEventListener('click', function () {
-
-    // Get random value by rolling dice
-    var dice = Math.floor((Math.random() * 6) + 1);
-
+    
+    // Roll both dice first
+    rollDice();
+    
     // Display dice image according to dice value
     document.getElementById('img').style.display = 'block';
-    document.getElementById('img').src = "dice-" + dice + ".png";
-
+    document.getElementById('img1').style.display = 'block';
+    document.getElementById('img').src = "dice-" + dice1 + ".png";
+    document.getElementById('img1').src = "dice-" + dice2 + ".png";
+    
     // Add the Localscore of an Active Player if it is not 1.
-    if (dice !== 1) {
+    if (dice1 == 1 || dice2 == 1) {
+        
+        // Local Score and Global Score for an active player will be 0
+        localScore = 0;
+        document.getElementById('local-' + activePlayer).textContent = localScore;
+
+        // Active toggle function called for changing the active player
+        activeToggle();
+    }
+    else {
+        
+        // Set this round local score as last local score
+        lastLocalScore = dice;
+        
+        // Add local scores
         localScore += dice;
         document.getElementById('local-' + activePlayer).textContent = localScore;
+    }
+    /*
+    if (dice !== 1) {
+        lastLocalScore = dice;
+        localScore += dice;
+        document.getElementById('local-' + activePlayer).textContent = localScore;  
     }
     else {
         // Local Score and Global Score for an active player will be 0
@@ -36,128 +53,189 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         document.getElementById('local-' + activePlayer).textContent = localScore;
         //globalScore[activePlayer] = 0;
         //document.getElementById('global-' + activePlayer).textContent = globalScore[activePlayer];
-
         // Active toggle function called for changing the active player
         activeToggle();
-    }
+    } */
 });
 
-// Hold button onclick event
+// 'Hold' button onclick event
 document.querySelector('.btn-hold').addEventListener('click', function () {
-
+    
+    // Dice display off
     diceOff();
-
+    
     // Store and display Localscore into Globalscore
     globalScore[activePlayer] += localScore;
     document.getElementById('global-' + activePlayer).textContent = globalScore[activePlayer];
-
+    
     // Localscore will be set to 0
     localScore = 0;
     document.getElementById('local-' + activePlayer).textContent = localScore;
-
+    
     // Check whether player has won?
     if (globalScore[activePlayer] >= winningTotal) {
-
-        document.querySelector('#name-' + activePlayer).textContent = "Winner";
+        
+        // Announce him winner
+        document.querySelector('#name-' + activePlayer).textContent = "Winner!!";
+        
+        // Assign the winner class
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-
+        
+        // Dice display off
         diceOff();
-
+        
+        // 'Rolldice' and 'Holdbutton' display off
         document.querySelector('.btn-roll').style.display = 'none';
         document.querySelector('.btn-hold').style.display = 'none';
     }
     else {
+        
+        // Interchange the active class
         activeToggle();
     }
 });
 
-// Newgame button onclick event
+// 'Newgame' button onclick event
 document.querySelector('.btn-new').addEventListener('click', newGame);
-document.querySelector('.btn-reset').addEventListener('click', function(){
+
+function newGame() {
+    
+    // Page redirect to Home Page
+    location.href = "HomePage.html"
+}
+
+// 'Reset' button onclick event
+document.querySelector('.btn-reset').addEventListener('click', function () {
+    
+    // assign  value to x for reset
     x = "reset";
+    
+    // resetGame event fire
+    resetGame();
 });
 
-function activeToggle() {
+// Resetgame event fire
+function resetGame() {
+    
+    askWinningScore();
+    function askWinningScore() {
+        winningTotal = parseInt(prompt("Please Enter Winning Score (between 10 to 1000)"));
+        document.getElementById('txtWinningTotal').textContent = 'Winning Total: ' + winningTotal;
+        
+        // Check the validation
+        if (isNaN(winningTotal) || winningTotal < 10 || winningTotal > 1000) {
+            alert("Are you dumb?? Please, read instruction carefully.")
+            askWinningScore();
+        }
+        else {
+            init();
+            document.getElementById('name-0').textContent = playerOneName;
+            document.getElementById('name-1').textContent = playerTwoName;
+        }
+    }
+}
 
+// function for interchange active class
+function activeToggle() {
+    
     // Which one is active player checked by using ternary operator
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-
+    
     // active class will be toggled between player 1 and 2
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 }
 
-/*function resetGame(){
-     winningTotal = parseInt(prompt("Please Enter Winning Score (between 1 to 1000)"));
+// fuction for initial prompts 
+function inquiries() {
+    
+    // Take input for winning score
+    winningTotal = parseInt(prompt("Please Enter Winning Score (between 10 to 1000)"));
+    document.getElementById('txtWinningTotal').innerHTML = 'Winning Total: ' + winningTotal;
+    
     // Check the validation
-    if (isNaN(winningTotal) || winningTotal < 1 || winningTotal > 1000) {
-        alert("Are you dumb?? Please, read instruction carefully.")
-        resetGame();
-    }
-    else {
-        document.getElementById('name-0').textContent = playerOneName;
-        document.getElementById('name-1').textContent = playerTwoName;
-    }
-    init();
-
-};*/
-
-function newGame() {
-
-    location.href = "HomePage.html"
-
-}
-
-function inquiries(){
-     // Take input for winning score
-    winningTotal = parseInt(prompt("Please Enter Winning Score (between 100 to 1000)"));
-    // Check the validation
-    if (isNaN(winningTotal) || winningTotal < 100 || winningTotal > 1000) {
+    if (isNaN(winningTotal) || winningTotal < 10 || winningTotal > 1000) {
         alert("Are you dumb?? Please, read instruction carefully.")
         inquiries();
     }
     else {
+        
         playerOneName = prompt("Enter Player One Name:");
-        if(playerOneName == ""){
-           playerOneName =  document.getElementById('name-0').textContent;
+        if (playerOneName == "") {
+            playerOneName = document.getElementById('name-0').textContent;
         }
-        else{
-             document.getElementById('name-0').textContent = playerOneName;
+        else {
+            document.getElementById('name-0').textContent = playerOneName;
         }
         playerTwoName = prompt("Enter Player Two Name");
-        if(playerTwoName == ""){
-             document.getElementById('name-1').textContent
+        if (playerTwoName == "") {
+            document.getElementById('name-1').textContent
         }
-        else{
-             document.getElementById('name-1').textContent = playerTwoName;
+        else {
+            document.getElementById('name-1').textContent = playerTwoName;
         }
-
-
     }
 }
 
-function init(){
-
-     // Initialize variables
+//function for intialization
+function init() {
+    
+    // Initialize variables
     globalScore = [0, 0];
     localScore = 0;
     activePlayer = 0;
+    lastLocalScore = 0;
+    dice = 0;
+    dice1 = 0;
+    dice2 = 0;
+    x = "";
+    
     // Display all score zero
     document.getElementById('local-0').textContent = '0';
     document.getElementById('global-0').textContent = '0';
     document.getElementById('local-1').textContent = '0';
     document.getElementById('global-1').textContent = '0';
+    
     diceOff();
+    
     // Display on for roll and hold button
     document.querySelector('.btn-roll').style.display = 'block';
     document.querySelector('.btn-hold').style.display = 'block';
+    
     // Add and remove active class from Player 1 to Player 2 respectively
     document.querySelector('.player-0-panel').classList.add('active');
     document.querySelector('.player-1-panel').classList.remove('active');
-
 }
+
 function diceOff() {
+    
     // Dice display off
     document.getElementById('img').style.display = 'none';
+    document.getElementById('img1').style.display = 'none';
+}
+
+function rollDice() {
+    
+    // Get random value by rolling dice
+    // dice = Math.floor((Math.random() * 6) + 1);
+    dice1 = Math.floor((Math.random() * 6) + 1);
+    dice2 = Math.floor((Math.random() * 6) + 1);
+    dice = dice1 + dice2;
+    
+    checkDiceScore();
+}
+
+function checkDiceScore() {
+    
+    // If Last Score and this Local Score is 12 then global and local both will be 0
+    if (lastLocalScore === 12 && dice === 12) {
+        // Local Score and Global Score for an active player will be 0
+        localScore = 0;
+        document.getElementById('local-' + activePlayer).textContent = localScore;
+        globalScore[activePlayer] = 0;
+        document.getElementById('global-' + activePlayer).textContent = globalScore[activePlayer];
+        //Active toggle function calledfor changing the active player
+        activeToggle();
+    }
 }
